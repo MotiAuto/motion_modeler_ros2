26938f91cceb3f909a2536e8b8c1fc95080702f4
motii8128 <default270robo@gmail.com>
2024-05-07 23:05:02 +0900

    Update

---
+++ src/bin/sado.rs
@@ -0,0 +1,36 @@
+use safe_drive::{
+    context::Context, error::DynError, logger::Logger, msg::{common_interfaces::{geometry_msgs, std_msgs}, RosString}, pr_info
+};
+
+use motion_modeler_ros2::Wheel2;
+
+fn main()->Result<(), DynError>
+{
+    let ctx = Context::new()?;
+    let node = ctx.create_node("sado_motion_modeler", None, Default::default())?;
+    let mut selector = ctx.create_selector()?;
+    let log = Logger::new(node.get_name().unwrap().as_str());
+
+    let subscriber = node.create_subscriber::<geometry_msgs::msg::Twist>("/cmd_vel", None)?;
+    let publisher = node.create_publisher::<std_msgs::msg::String>("/wheel", None)?;
+
+    selector.add_subscriber(
+        subscriber, 
+    Box::new(move |msg|{
+        let mut wheel2 = Wheel2::new(0.0, 0.0);
+
+        wheel2.left = (-msg.linear.y) as f32;
+        wheel2.right = (-msg.linear.y) as f32;
+
+        let mut send_msg = std_msgs::msg::String::new().unwrap();
+        send_msg.data = RosString::new(wheel2.serialize().as_str()).unwrap();
+
+        let _ = publisher.send(&send_msg).unwrap();
+    }));
+
+    pr_info!(log, "Start OmniMotionModelerROS2");
+
+    loop {
+        selector.wait()?;
+    }
+}
